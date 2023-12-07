7b19b22b9d05f4c46904faf082879ad54a267b8f
Aayushman Sharma <075bme004aayushman@gmail.com>
2023-12-07 14:25:01 +0545

    Sprite.js file is added, wherein Sprite class is made so that the particular portion of the spritesheet can be extracted

---
+++ scripts/Sprite.js
@@ -0,0 +1,9 @@
+class Sprite{
+    constructor(image, sx, sy, sw, sh){
+        this.image = image;
+        this.sx = sx;
+        this.sy = sy;
+        this.sw = sw;
+        this.sh = sh;
+    }
+}
